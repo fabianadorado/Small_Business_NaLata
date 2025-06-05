4005d2ad2b58ec4ae9aa77b56784131304dc5100
fdorado <fdpvieira@gmail.com>
2025-06-05 22:11:32 +0100

    Adiciona .gitignore para ignorar arquivos temporários e de build

--- produtos.cpp
+++ produtos.cpp
@@ -1,1 +1,18 @@
+#include "Produto.h"
+#include <iostream>
+using namespace std;
 
+
+int Produto::proximoId = 1;
+
+Produto::Produto(std::string nome, int quantidade, float precoCusto)
+    : id(proximoId++), nome(nome), quantidade(quantidade), precoCusto(precoCusto) {
+}
+
+int Produto::getId() const { return id; }
+string Produto::getNome() const { return nome; }
+int Produto::getQuantidade() const { return quantidade; }
+float Produto::getPrecoCusto() const { return precoCusto; }
+float Produto::getPrecoVenda() const { return precoCusto * 1.3f; }
+void Produto::adicionarStock(int qtd) { quantidade += qtd; }
+void Produto::removerStock(int qtd) { if (quantidade >= qtd) quantidade -= qtd; }
