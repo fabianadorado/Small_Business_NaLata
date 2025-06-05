222a17a271abfce5d81e893d733e8f012e1474b7
gqmendes <143037224+gqmendes@users.noreply.github.com>
2025-06-05 23:08:31 +0100

    Update cliente.h
    atualizado client.h

--- cliente.h
+++ cliente.h
@@ -1,1 +1,36 @@
+#pragma once
+#include<iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+
+class Cliente
+{
+private:
+	static int proximoCliente;
+	int idCliente;
+	string nome;
+	string telefone;
+	string morada;
+
+public:
+
+	Cliente();
+	Cliente( string nome,string telefone, string morada);
+	
+
+
+	int getIdCliente();
+	string getNome();
+	string getTelefone();
+	string getMorada();
+
+	//void setIdCliente(int idCliente);
+	void setNome(string nome);
+	void setTelefone(string telefone);
+	void setMorada(string morada);
+	
+	string toString();
+};
 
